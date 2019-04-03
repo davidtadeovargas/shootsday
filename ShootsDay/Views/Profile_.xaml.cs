fad9d08785a81174776aba60792fd2cc503ee936
David Vargas <davidv@exigo.com>
2019-04-03 11:18:08 -0600

    - *Actividad: En el menu lateral donde dice Sesion de fotos que diga Red Social y abra la pantalla de Red Social (listo)
    -
    *Actividad: Crear una tabla en la base de datos para relacion de usuarios por eventos (listo)
    
    -
    *Actividad: Cambiar el texto del numero de contactos por evento a "Contactos en este evento: ?" (listo)
    
    -
    *Actividad: El texto donde dice 0 contactos en la pantalla de mi perfil colocar el total de usuarios que hay en ese evento (listo)
    
    -
    *Actividad: En el total de eventos del perfil de usuario, que no lo muestre cuando se esta consultado un perfil de otro usuario que no es el actual (listo)

--- ShootsDay/Views/Profile_.xaml.cs
+++ ShootsDay/Views/Profile_.xaml.cs
@@ -19,43 +19,57 @@ namespace ShootsDay.Views
         string host;
 
         public int userId { get; set; }
+        public int eventId { get; set; }
+        public bool anotherProfile { get; set; }
 
 
 
         public Profile_(int userId_)
         {
-            init(userId_);
+            init(userId_, -1,true);
         }
 
 
         public Profile_()
         {
-            init(-1);
+            init(-1,-1,false);
         }
 
-        private void init(int userId_)
+        private void init(int userId_, int eventId_, bool anotherProfile_)
         {
             InitializeComponent();
 
             Title = "Perfil de usuario";
 
             userId = userId_;
+            eventId = eventId_;
+            anotherProfile = anotherProfile_;
 
             get_profile();
+
+            //Do not show the users total when it is another profile and not the current
+            if (anotherProfile)
+            {
+                contacts.IsVisible = false;
+            }
         }
 
 
         private async void get_profile()
         {
             string username = SettingsManager.Instance.getUserName();
-            string password = SettingsManager.Instance.getPassword();
-            int id_event = SettingsManager.Instance.getIdEvent();
+            string password = SettingsManager.Instance.getPassword();            
 
             if (userId==-1)
             {
                 userId = SettingsManager.Instance.getUserId();
             }
-            
+
+            if (eventId == -1)
+            {
+                eventId = SettingsManager.Instance.getIdEvent();
+            }
+
             try
             {
                 LoadingManager.Instance.showLoading();
@@ -63,10 +77,11 @@ namespace ShootsDay.Views
                 var client = new HttpClient();
                 var userData = Newtonsoft.Json.JsonConvert.SerializeObject(new
                 {
-                    Event = new { id = id_event },
+                    id = userId,
+                    Event = new { id = eventId },
                     Login = new { password = password, username = username }
                 });
-                //var userData = Newtonsoft.Json.JsonConvert.SerializeObject( new { User = auxUser, Login = auxLogin } );
+                
                 var content = new StringContent(userData, Encoding.UTF8, "application/json");
 
                 var url = ImagesManager.Instance.getProfilePicture(userId);
@@ -112,7 +127,7 @@ namespace ShootsDay.Views
         {
             name.Text = profile.User.name + " " + profile.User.lastname;
             event_.Text = profile.Event.title;
-            contacts.Text = profile.Event.count_users.ToString() + " contactos";            
+            contacts.Text = "Contactos en este evento: " + profile.Event.count_users.ToString();
         }
     }
 }
